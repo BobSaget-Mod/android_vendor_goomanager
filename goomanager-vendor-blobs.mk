5ca17cfbf87c0d6c3e9592597232350f131b2beb
Paul Beeler <sparksco@gmail.com>
2012-09-27 04:43:28 -0600

    Customize for aokp

--- goomanager-vendor-blobs.mk
+++ goomanager-vendor-blobs.mk
@@ -1,8 +1,15 @@
+# Version information used on all builds
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+
+DATE = $(shell vendor/aokp/tools/getdate)
+
+# AOKP_BUILD=<goo version int>/<build string>
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=sparksco \
     ro.goo.board=$(TARGET_PRODUCT) \
-    ro.goo.rom=sparkscoCM10Nightlies \
-    ro.goo.version=$(shell date +%s)
+    ro.goo.rom=sparkscoAOKPNightlies \
+    ro.goo.version=$(shell date +%s) \
+    ro.aokp.version=$(TARGET_PRODUCT)-jb-$(shell date +%s)-$(shell date -u +%m%d)
 
 PRODUCT_COPY_FILES +=  \
     vendor/goomanager/proprietary/app/goomanager.apk:system/app/goomanager.apk
