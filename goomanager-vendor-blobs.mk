1c00a219d5c9340e908a2af9d6f81c147ff36ae9
Paul Beeler <sparksco@gmail.com>
2012-11-22 06:14:12 -0700

    Use Android.mk method for building prebuilt apk

--- goomanager-vendor-blobs.mk
+++ goomanager-vendor-blobs.mk
@@ -1,12 +1,8 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=sparksco \
     ro.goo.board=$(TARGET_PRODUCT) \
-    ro.goo.rom=sparkscoCM10Nightlies \
+    ro.goo.rom=sparksco-AOSP-4.2_r1
     ro.goo.version=$(shell date +%s)
 
-PRODUCT_COPY_FILES +=  \
-    vendor/goomanager/proprietary/app/goomanager.apk:system/app/goomanager.apk
-
 PRODUCT_PACKAGES += \
-goomanager \
     goomanager.apk
---
+++ proprietary/app/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := goomanager.apk
+LOCAL_SRC_FILES := goomanager.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_PATH := $(TARGET_OUT)/app
+include $(BUILD_PREBUILT)
