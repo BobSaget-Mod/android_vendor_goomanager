d53d5d19fbd75b2eb8c97650d0d48e8896093bc1
Paul Beeler <sparksco@gmail.com>
2012-12-02 12:52:40 -0700

    Update to 4.2.1_r1

--- goomanager-vendor-blobs.mk
+++ goomanager-vendor-blobs.mk
@@ -1,7 +1,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=sparksco \
     ro.goo.board=$(TARGET_PRODUCT) \
-    ro.goo.rom=sparksco-AOSP-4.2_r1 \
+    ro.goo.rom=sparksco-AOSP-4.2.1_r1 \
     ro.goo.version=$(shell date +%s)
 
 PRODUCT_PACKAGES += \
