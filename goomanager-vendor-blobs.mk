83df7227e8f608b940a35bc5fda84274a8b118dd
Paul Beeler <sparksco@gmail.com>
2013-02-15 00:01:47 -0700

    Stop including goomanager prebulit app in the ROMS

--- goomanager-vendor-blobs.mk
+++ goomanager-vendor-blobs.mk
@@ -1,8 +1,5 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=sparksco \
     ro.goo.board=$(TARGET_PRODUCT) \
-    ro.goo.rom=sparksco-AOSP-4.2.1_r1 \
+    ro.goo.rom=SaberMod-4.2.2_r1 \
     ro.goo.version=$(shell date +%s)
-
-PRODUCT_PACKAGES += \
-    goomanager
