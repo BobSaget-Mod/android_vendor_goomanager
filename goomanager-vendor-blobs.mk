5ebe2f6c7c9ded4c29c98b6b3fa5245aa86bd9cb
Paul Beeler <sparksco@gmail.com>
2013-04-20 15:07:18 -0600

    update to 4.2.2_r1.2 Signed-off-by: Paul Beeler <sparksco@gmail.com>

--- goomanager-vendor-blobs.mk
+++ goomanager-vendor-blobs.mk
@@ -1,5 +1,5 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=sparksco \
     ro.goo.board=$(TARGET_PRODUCT) \
-    ro.goo.rom=SaberMod-4.2.2_r1 \
+    ro.goo.rom=SaberMod-4.2.2_r1.2 \
     ro.goo.version=$(shell date +%s)
